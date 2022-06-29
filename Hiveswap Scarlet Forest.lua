7f20060a3d6d3108d2ea65faf496edc30e71bd7f
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 12:09:41 -0400

    Add files via upload

---
+++ Hiveswap Scarlet Forest.lua
@@ -0,0 +1,72 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*13 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+6 [ute]|[ute] 6 [ute] 3 [ute]
+3 [wur]|[wur] 3 [wur] 7 [wur]
+2 [yqe]|[yqe] 2 [yqe] 6 [yqe]
+5 [ywr]|[ywr] % [yrW] 3 [yrW]
+6 [ute]|[ute] 6 [ute] 3 [ute]
+5 [wut]|[wut] 5 [wut] 2 [wut]
+$ [teQ]|[teQ] $ [teQ] 2 [teQ]
+4 [ute] 1 [ute] % [urW] 3 [urW]
+[a6] [t0]|[tp0] [a6] [t0] 6 [tp0]
+[u0] [wr]|[ywr] [u0] [wr] 7 [wur]i
+[o9] [qe]|[qie] [o9] [qe] 6 [qie]
+[y5] [r9]|[tr9] [y%] [W7] 3 [uW7]y
+[t6] [e0] u [t0] [a6] [t0] [s6] [t0]
+[d5] [t0] p [t0] [a5] [t0] [s9] [t0]
+[fQ] [te]|[te] Q [te] 9 [ted]s
+[qf] [te] 8 [ted]s[fW] [yr] 0 [yr]
+[a6] [t0]|[tp0] [a6] [t0] 6 [tp0]
+[u0] [wr]|[ywr] [u0] [wr] 7 [wur]i
+[o9] [qe]|[qpe] [o9] [qie] [u6] [qie]
+[y5] [wt9] y [wu9] [y%] [W7] 3 [uW7]y
+[t6] [e0] u [t0] [a6] [t0] [s6] [t0]
+[d5] [t0] p [t0] [a5] [t0] [s9] [t0]
+[fQ] [te]|[te] Q [te] 9 [tje]k
+[qlf] [te] 8 [tke]j[dWH] [yr] 0 [yr]
+[skf6] t 0 [tj] [skf6] t 0 [tj]
+[ofa0] r w [rd] [ofa0] r w [rf]g
+[phd9] e q [ge] [phd9] e q [ge]
+[oda5] r 9 [sr] [daO%] r 0 [rf]d
+[sp6] t [f0] t [skf6] t [l0] t
+[zhf5] t [j0] t [khf5] t [l0] t
+[xjG$] t 0 t $ t 0 [tj]k[ljf4] e 8 [ke]j[fdH3] W 7 W
+6 t 0 t 6 [tl] [x0] [tm]
+[vn0]mnr w [xrb] [vn0] r w [rn]m
+[zc9] e q [xme] [zn9] [leb] [vqk] [leb]
+[vk5]bv[rc] [x9] [zr] [x%] [rV] [n0] [zr]
+[xb6] [tj] [mb0] [xt] [zn6] [xt] [xm0] [xt]
+[vn5]mnt 0 [tcb] [xv5] [ztc] [xl0] [ztc]
+[lb$] t 0 t $ t 0 [tb]n[xm4] e 8 [ne]b[zV3] W 7 W
+9 [qe] u [qie] [p9] [qie] [u6] [qie]
+[s9] [qie] u [qie] [s9] [qea]sa[p6] [qea]
+[s6] [t0]|[t0] [f6] [tdQ]fd[s3] [tdQ]
+[f6] [vt0] CvC[zt0] [x6] [tQ] [b3] [tQ]
+9 [qe] u [qie] [p9] [qie] [u6] [qie]
+[sf9] [qie] u [qie] [sf9] [qed]fd[s6] [qed]
+[sf6] [t0]|[xtl0][zc][xv6] [xvt0] [zv3] [zvt0]
+[vl6] [t0] k [t0] [jb6]|[nk5]|
+9 [qe] u [qie] [p9] [qie] [u6] [qie]
+[s9] [qie] u [qie] [s9] [qea]sa[p6] [qea]
+[s6] [t0]|[t0] [f6] [tdQ]fd[s3] [tdQ]
+[f6] [vt0] CvC[zt0] [x6] [tQ] [b3] [tQ]
+9 [qe] u [qie] [p9] [qie] [u6] [qie]
+[sf9] [qie] u [qie] [sf9] [qed]fd[s6] [qed]
+[sf6] [t0]|[xtl0][zc][xv6] [xvt0] [zv3] [zvt0]
+[vl6] [t0] k [t0] [sj6]|[kd5]|
+[lj4] [e8]|[e8] [lj4] [ke8]lk[j4] [ke8]
+[lH4] [W8]|[W8] [lH4] [kW8]lkj [kW8]
+[lh3] [w8]|[wk8] [hd3] [W7] 3 [gW7]
+[sf6] [e0]|[ed0] [s6] [ed0] [f6] [ge0]
+[hf9] [qhfe] [hd] [qhed] [sh9] [qe] [s9] [qfe]
+[da0] [wrda] [pd] [wrpd] [od0] [wr] [xl0] [zwrk]
+[qlj] [te]|[te] q [te] [qh] [te]
+[xol] [sfa] c [sfa] [zkO] [sfa] [lO] [sfa]
+6 [t0]|[t0] 6 [t0] 3 [80] 6
+]]
