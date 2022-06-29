f02a6fd2ce5d3ae38ae4e9c1ecabed4d8dab294b
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 14:24:38 -0400

    Add files via upload

---
+++ adieu to the paino.lua
@@ -0,0 +1,61 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+u w|
+[u851] w 0 w [y7] w
+[t851] w 0 w t y
+[u851] w 0 w [y7] w
+[t8] w Q w t y
+[u73] r W r u I
+[O730] r W r O r
+[O73] r E r I r
+[u730] r W r 0|
+[u851] w 0 w [y7] w
+[t851] w 0 w t y
+[u851] w 0 w [y7] w
+[t8] w Q w t y
+[u73] r W r u I
+[O730] r W r O r
+[O73] r E r I r
+[u81] t w t u I
+[o7] y r y w o
+[t6] e 0 e t y
+[u5] r w r 0 u
+[e4] q 8 q e r
+[t3] w [r2] w [t1] w
+[yr7] w [yi5] w [ut1] [w3]
+[yr5] w Q w re w
+[u851]||w|0|w|[y7] w
+[t851]||w|0|w|t y
+[u851]||w|0|w|[y7] w
+[t851]||w|Q|w|t u
+[o1]50| t|r|t|[oE510]|t|
+[qoe4] t [ie] t [yq4] e
+[t50] w [yq] w [rq5] w
+[t10]|1||
+[u81] t w t u I
+[o7] y r y w o
+[t6] e 0 e t y
+[u5] r w r 0 u
+[e4] q 8 q e r
+[t3] w [r2] w [t1] w
+[yr7] w [yi5] w [ut1] [w3]
+[yr5] w Q w re w
+[u851]||w|0|w|[y7] w
+[t851]||w|0|w|t y
+[u851]||w|0|w|[y7] w
+[t851]||w|Q|w|t u
+[o1]50| t|r|t|[oE510]|t|
+[qoe4] t [ie] t [yq4] e
+[t50] w [yq] w [rq5] w
+[t10]|1|o|
+[t80]uow 5 w [s80] w
+[qoia9] w 5 w [utso80] w
+[yd97]iow [a5] w [o97] w
+[i80] w 5 w [u80]
+]]
