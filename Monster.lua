e693c281185097878d2dc73f9deaa6a0cf0acf95
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:48:52 -0400

    Add files via upload

---
+++ Monster.lua
@@ -0,0 +1,73 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*12 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[hed][jf]u [pjf]|
+[jf]| [jf][ha]|q t i|
+wyoyr[wa][d9][h5]
+[hd6][jf]0 [jfe] r
+[tjf]| [kh][hd]|4 8 q|
+59weryoaej[uj]j[pj]f[fa]f[sj]
+j f[xm]nbq [tj]j[ji] f
+w [ysg] [of] asdfe[kf][ukf][kf][pkf]jh
+[qf]h [th]h[ih] [xh]
+[zwh] [yh]h[oh]|w[f0]g[wh]g[rf]
+0wru[utje]jhj
+uoa[tqje]jhj
+psf[ywrhd][hd][hd][hd][hd][jf]
+[ked]tu [hd]|[ts][ra][pe]
+[utpje][pj][oh][pj]xvn
+[tqpje][pj][oh][pj]bmx
+[ywr]ljj[ukh]h|
+[utfe]fhh[ywrj] l[uj]
+[sqj][xji]j[qji]j [wof]hj|
+jjjh[hd0][rhd][hd][uh]k [utle][jf]
+nmz[xute][tj][rj][je]
+[qjf][xtl][jf][qi]j [ywoh]j|
+fdaoys[jif]t[qf][ywrd] S[yd][rS]
+eTyu[pjf]u[jf]y[lT]
+[qjf]et[qi][jf][jf]l
+[wjf]ry[wo]|gg
+[jd0][wjd][rjd][ujd0]k [utkhed]|
+[tslf][ra][plfe][wo]
+qt[sqjif]t q[tsj]i
+wy[wsojfd]y w[ysj]o
+0r[usjf0][rh] 0[rj][wj]
+[utje]xj[uj][tj]0[je80]6|||
+[ute]| [xh]zl
+[ywrkh]| wyo
+[tqe]| [ytd]g[je]
+[rWH0]|0rW07
+[qe9]dd[qd][rf]dqs
+[srW0]a|0ur[wh]r
+[utjfe] [jf]e[ut]rle
+[zqje9] [zj]9[qe]0v9
+[uteb]vx[xs][za][pl][pl][ok][ji]
+[zxrkW0] [zxk0]ru[zxrk0] [r30]
+xnvxkhfaor|||
+j[ji]x[sj]j[jg] [of]ahdj|
+jjjh[uh]h[ha]h[kf] lj
+p f oj[jd]j
+[ji]x[sj]j[jg] [oh]j
+d h d|s[qj] [tf]d5S[d9]S
+6 0 [jfe60][jf][wlf95]
+[qjf84] [xlb][q84][jf][jf]q[lf8]
+[wjf95] [xvn]wyo[ysrg][sg]
+[sj3][j7][sj0][j730][khf] [khfe60]|
+[t8][r7][e6][wslf5] [slf]
+48[sqpf4]8 4[slf8]q
+59[wlgf5]9 5[zjg9]w
+37[xj30][zjh7] [x3][j7][j5]
+[j860]xj[j3][j8]0[le80][l6]|
+ll e[ul][uplje]|
+l[lj]qt[ki][ywokh]|l
+[sjfe]upapute
+[ql]btvi[xo][xp]zllkj
+[xwj]yopa z
+[khfe]uphasdf
+[phf]sfj
+]]
