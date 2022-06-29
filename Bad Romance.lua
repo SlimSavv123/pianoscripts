a8a13628134d2cba0d98535a2022881356b3c2e1
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:51:28 -0400

    Add files via upload

---
+++ Bad Romance.lua
@@ -0,0 +1,85 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*32 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+e|e|u u i u|
+e e|u u i u
+e|e|u u i u|
+t t e t e t|
+[e6]|[e6]|[u3] [u3] [i4] [u3]|
+[e6] [e6]|[u3] [u3] [i4] [u3]
+[e6]|[e6]|[u3] [u3] [i4] [u3]|
+[t1] [t1] [e6] [t1] [e6] [t1]|
+6 w e [w6] [e6] e|e
+[t6] e w [e6] 8 w|
+6 e e [w6] [e6] w e w
+[t6] e w [e6] 8 0 w e
+[e6]| 6 6||
+[u8]|t 8 [e9] 0 w e
+[e6]| 6 6||
+8| 8 8|7|
+6 t t [e6] [t6] t|e
+[t6] t e [t6] 8||
+6 t t [e6] [t6] e t e
+[t6] e e [e6] [w8] 0 w e
+[e6]| 6 6||
+[u8]|t 8 [e9] 0 w e
+[e6]| 6 6||
+6| 6 6|5|
+4 e e e [e4] e [w4] w
+[w5] w w 9 5 9 [95] 0
+6 0 0 0 [w6]|[60] 9
+[81]| 1 [81] 9 [10] 8
+[q4] e e e [e4] e [w4] w
+[w5] w w 9 5 9 [95] 0
+3 0 0 0 [W3]|[30] 9
+[86]| 6 [t6] y [u6] t
+[i84]| u [i84] u y|
+[95]| r [t95]|y|
+[u60]|u u [u60] y t t
+[w8]||[t51] y u t
+[i84]| u [i84] u y|
+[95]| r [t95]|y|
+[u73]|u u [u73] y t t
+[60]
+[e6]|[e6]|[u3] [u3] [i4] [u3]|
+[e6] [e6]|[u3] [u3] [i4] [u3]
+[e6]|[e6]|[u3] [u3] [i4] [u3]|
+[t1] [t1] [e6] [t1] [e6] [t1]|
+6 w e [w6] [e6] e|e
+[t6] e w [e6] 8 w|
+6 e e [w6] [e6] w e w
+[t6] e w [e6] 8 0 w e
+[e6]| 6 6||
+[u8]|t 8 [e9] 0 w e
+[e6]| 6 6||
+8| 8 8|7|
+6 t t [e6] [t6] t|e
+[t6] t e [t6] 8||
+6 t t [e6] [t6] e t e
+[t6] e e [e6] [w8] 0 w e
+[e6]| 6 6||
+[u8]|t 8 [e9] 0 w e
+[e6]| 6 6||
+6| 6 6|5|
+4 e e e [e4] e [w4] w
+[w5] w w 9 5 9 [95] 0
+6 0 0 0 [w6]|[60] 9
+[81]| 1 [81] 9 [10] 8
+[q4] e e e [e4] e [w4] w
+[w5] w w 9 5 9 [95] 0
+3 0 0 0 [W3]|[30] 9
+[86]| 6 [t6] y [u6] t
+[i84]| u [i84] u y|
+[95]| r [t95]|y|
+[u60]|u u [u60] y t t
+[w8]||[t51] y u t
+[i84]| u [i84] u y|
+[95]| r [t95]|y|
+[u73]|u u [u73] y t t
+[60]
+]]
