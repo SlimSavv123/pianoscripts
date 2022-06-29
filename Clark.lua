10d5c9e129a08d43a468b464227654a246c15b15
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 12:32:49 -0400

    Add files via upload

---
+++ Clark.lua
@@ -0,0 +1,24 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*40 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[ypi] [ur0] [wur] [tqe] [yqpe9] t [ur0] w
+e u [wur0] [tqe] e [yqpe9] o p
+o [ur0] w e [wur0] s a o
+y [tqe] e y i o [ypi2] i
+u y 4 [up5] o [ura30] [ws] [a5]
+p e o [wur3] a s a o
+[y6] [a8] s [tqe4] [ute] o y [pi3]
+[oa] [ypi2] i s [uo] y 4 [up5]
+o [ura30] [ws] [a5] p e o y
+[wur30] a g f s [p6] [a8] s
+[tqe4] [ute] o y [pi3] [oa] [ypi2] g
+o f d [p4] [f5] [od] [ura30] [ws]
+[a5] p e o y [wur3] k [lf]
+[kd] [sh] [j6] [kf8] l [spi4] [ute] o
+y [pi3] [ua] [yqe] [ur0] [wur] [tqe]
+]]
---
+++ Living Mice.lua
@@ -0,0 +1,44 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*38 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+w e u t u w e u
+t w e u t u w e
+u t w e u t u w
+e u t w e u t u
+w e u t [wp] e u t
+[us] a [o0] [pe] u [td] t [wp]
+e u t [uf] d [wp] [fe] u
+[ts] [s9] [ed] [uf] t u s d
+[p9] e u t u [h9] e [uf]
+[ts] u h [d9] [pe] u t u
+[wsp6] e a u [ut] o s a
+8 [wuo6] [pe] u [td] t [wsp6] e
+f u [tp] u f d 0 [wpd6]
+[fe] u [tsp] [wsp6] e a u [ut]
+o s a 8 [wuo6] [pe] u [td]
+t [wsp6] e f u [tp] u f
+d 0 [wpd6] [fe] u [tsp] [yws4] [ue]
+[ua] [to] [fa] d 8 [yw3] [ue] [ua]
+[tod] t [wo5] [pe] [uf] [ts] [uf] d
+9 [wo$] [pe] [uf] [ts] [wpd4] [fe] [uk]
+[th] [ukf] s 8 [wda3] [fe] [uk] [thd]
+[ywo5] [pe] [uf] [ts] [uf] d 9 [ywo$]
+[pe] [uf] [ts] l z [xwlhf6] e [sl]
+u [tka] [oh] u [utf] 0 [xwf5] e
+[sl] u [tka] [xwljf6] e [zd] u [tpj]
+[IG] [uf] [yed] [ra] [xwf8] e [zd] u
+[tsj] [xwlhf6] e [sl] u [tka] [oh] u
+[utf] 0 [xwf5] e [sl] u [ztd] [xwljf6]
+e [zd] u [tsj] [IG] [uf] [yed] [ra]
+[xwf8] e [zd] u [tpj] [wo6] [pe] [uf]
+[ts] [uf] [wo5] [pe] [uf] [ts] [wo6] [pe]
+[uf] [ts] [uf] [wo8] [pe] [uf] [ts] [wo]
+[pe] [uf] [ts] [uf] [wo] [pe] [uf] [ts]
+[wo] [pe] [uf] [ts] [uf] [wo] [pe] [uf]
+[tso]
+]]
