f0a8314710d1bf3c81afa1b684d78b481e8bd886
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:56:51 -0400

    Add files via upload

---
+++ Anpanman.lua
@@ -0,0 +1,50 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*40 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[8f]|f|s||[4p]|p a s|p|
+[5d]| f d|s|[5a]|p|o||
+[8f]|f|s||[4p]|p a s|p|
+[5d]| f d|a|[8s]|||
+[4p]|s|[3o]|s|[5i] u i o u|t|
+[5y]|e e y|p|[5o]|y y o y o a
+[4p]|s|[3o]|s|[5i] u i o u|t|
+[5i] u i o u y u i [5y] e r t r|o|
+[8t] y u i o|s|[4p]|o i o||
+[5i]|u y u|t|[5y]| u y||
+[8t] y u i o|s|[4p]|o i o||
+[5i]|u y u|t|[5y]| o t||
+[7y] u y|y u y|[7y]|o|y||
+[8u] i u|u i u|[8u]|p|u||
+[0o] p o|o p o|[qo]|s|o|f|
+[wd]|p|a| p o|[wo]|[ep]|[ra]|
+[tf]||[es]||[qp]|p a s|p|
+[wd]| f d|s|[wa]|p|o||
+[tf]||[es]||[qp]|p a s|p|
+[wd]| f d|a|[ts]|||
+[4p]|s|[3o]|s|[5i] u i o u|t|
+[5y]|e e y|p|[5o]|y y o y o a
+[4p]|s|[3o]|s|[5i] u i o u|t|
+[5i] u i o u y u i [5y] e r t r|o|
+[8t] y u i o|s|[4p]|o i o||
+[5i]|u y u|t|[5y]| u y||
+[8t] y u i o|s|[4p]|o i o||
+[5i]|u y u|t|[5y]| o t||
+[7y] u y|y u y|[7y]|o|y||
+[8u] i u|u i u|[8u]|p|u||
+[0o] p o|o p o|[qo]|s|o|f|
+[wd]|p|a| p o|[wo]|[ep]|[ra]|
+[tf]||[es]||[qp]|p a s|p|
+[wd]| f d|s|[wa]|p|o||
+[tf]||[es]||[qp]|p a s|p|
+[wd]| f d|a|[ts]|||
+[qp]|s|[0o]|s|[wi] u i o u|t|
+[wy]|e e y|p|[wo]|y y o y o a
+[qp]|s|[0o]|s|[wi] u i o u|t|
+[ry]|[8e]|||
+[osfl]|[osfl]
+]]
