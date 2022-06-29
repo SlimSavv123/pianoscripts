d0a6c21fa98eae31edbc5eb56996ee7acbbbdcf3
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:06:57 -0400

    Add files via upload

---
+++ Naruto alone.lua
@@ -0,0 +1,24 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*24 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+o|
+[f8] w t u [d7] 9 w [rf] [s6] 0 e r t e [wr]|
+[qj] 8 e t [wh] 9 w [rj] [fe] 0 e r t e 0 8
+[tf] w t u [d7] 9 w [h9] [s6] 0 e r t e [ur]|
+[qj] 8 e t [wk] 9 r [h9] [je] 0 e r t [sqj] 8 e q t q [lig] q [wkd] 9 w r o 9 [wha] 9
+[sqj] 8 e q t q [lig] q [wkd] 9 w r o 9 w 9
+[sqj] 8 e q t q [lig] q [wkd] 9 w r o [rk] [wl] [z9]
+[qljg] 8 e t i [ql] [ze] [xq] [wh][zr][yv] o|
+[tsf] w t u [wda] 9 w [yf] [sp6] 0 e r t e [wr]|
+[qjg] 8 e t [whd] 9 w [rj] [sfe] 0 e r t 0 u 0
+[sf8] w t u [da7] 9 w [rh] [sp6] 0 e r t e [wr]|
+[qjg] 8 e t [wkh] 9 r [h9] [jfe] 0 e r t e 0 6
+[qjg] 8 e q [wkh] 9 w [rh] [jfe] 0 e r t 0 u 0
+[qjg] 8 e q [wkh] 9 w [rh] [sjf6] 0 e 0 t 0 e 0
+[fe][tj][ul]
+]]
