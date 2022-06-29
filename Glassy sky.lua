184a5a160d1ab626ebd05d5a05306003cb16bb4d
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:14:16 -0400

    Add files via upload

---
+++ Glassy sky.lua
@@ -0,0 +1,87 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 4*59 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+k s k h k| |
+k l v|v|v|b|x||
+z x k|k|||
+[qf] e [tp] s f j x z| |
+[ute]|f|k l b b v z|
+j z [ztqe] x|j| z| |
+[ute]| k l n|n|v z|x|
+[tqe]|h h h|h|g f|d f
+[ute]| z x k f j k| l k|
+[tqe]|h h h|h|g f|d f
+[ute] h f|[xvn]|[xvn]|x|x z|
+[tqe]|h h h h [urj0]|f| d f
+[ute]| k|l|k| f|s d
+[tqe]|f p|s d z h|a|s|
+[uthe]||j f| |
+[d4] o [f8] p [qh] h|h 4|[g8] f q|d f 6|0|[pe]|d|[f6]|[p0]|e|
+[d4] o [f8] p [qh] h|h 3|[g7] f 0|d f 6 h [f0]|[pe]|d|[f6]|[h0]|[je]|
+[d4] o [f8] p [qh] h h [j3]|[f7]|0|f|
+[k6]|[j0]|[ke] j|f 9|e|y|
+[pj4]|8|[xqf]| [vh4]|8|[qjb]|
+[nk3]|7|0|[ml] [nk] 3|7|[u0] f|
+[pid4]|8|q|p [oda] 5|9|w|
+6|0 d [ed] d|d 3|[s7] a 0|
+u a 4|8|q|s p 5|9|[wo]|
+[u6]|[r0] t e [ut]|[to] 6 [tp] 0 u e f|
+[pid4]|8|q|p [udO] 3|7|0|a
+[usp6]|0|[uspe]| [aI9]|[pe]|[yo]|p|
+[u4]|8|[qoh]| [pj4]|8|[sql]|
+[zd5]|9|[xwf]| [jb5]|9|[wvh]|b
+[wjb50]||v [xj]| [zj] x k|
+[ke] u p f|l j| |
+[qf] j t h i d f j j| h d s 6 0 r t a s d|d| |
+[tqpe]|a|s|d|[yrd0]|f|a|s
+[ed] r t u|h|[sje9]| [sj] k l
+[sqjf] e t u i p s f|[xj]|z|k|
+[ke] u o a|h f| |
+[tqe]|h h h|h|g f|d f
+[ute]| k|h|h|h|h j f|
+[tqe]| h g h|h|g f|d f
+[ute] h f| [zjf]|[kjf]|[ljf]|j|h|
+[tqe]| h g h|[urj0]|f| d f
+[ute]| l|k|[tjQ9]|G|f|s d
+[tqe]|f p||p a s|
+[uthe]||j f [ywr]| |
+6|0 [hf] [hfe] [hf]|[hf] 6|[gd0] [sf] e|d [fa]
+5|9|[wha]| [ka5]|9|[zwa]|
+[l4] k [j8]|[qhf] [hf]|[hf] 4|[gd8] [sf] q|d [fa]
+3|7|[pf0]| [fO3]|7|0|
+4|8 [hf] [qhf] [hf] [hf] [jf5]|[fa9]|w|d [sf]
+6|0|[le]|k|[j9]|[eG]|[yf]|s d
+4|[f8] p q| 4|8|[qa]|s|
+[h5]|9|w|j f 5|9|[wj] k x| |
+[tsqi]| |f||d s
+[wua]|s a| f| |
+[uqp]| p s|d| s a|s a [y0] o||p| p|f|
+[ted9]| p| f| s|s|
+[yrd0]||f j| h k|
+[utqljf]||k j| h j|
+[wupf]||d f|a|s|d|||
+[u30] f|
+[pid4]|8|q|p [oda] 5|9|w|
+6|0 d [ed] d|f 3|[s7] a 0|
+u a 4|8|q|s p 5|9|[wo]|
+[u6]|[r0] t e [ut]|[to] 6 [tp] 0 u e f|
+[pid4]|8|q|p [udO] 3|7|0|a
+[usp6]|0|[uspe]| [aI9]|[pe] o y|p|
+[up4]|8|[qoh]| [pj4]|8|[sql]|
+[zd5]|9|[xwf]| [jb5]|9|[wu] f|
+[tqd]||f p [wu]| o p|
+[ute]||y u|r|t|y| |u| |
+q t o p|f d| |
+d w p| h d||
+d f e i p s|l k|[lf]|[sk]|
+[rkh]|o|a| d||d f
+[tl] d f k d f h o|d|
+[qd] t o p s d h s d z|
+[xe] u p [lf]| [kf]||l|
+[ql]|o|[spf]| [sjf]| [zf]| [vj]
+]]
