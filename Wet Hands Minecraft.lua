8b79779f10e08fa7dad5f275ea04a3e983b196b6
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 12:15:50 -0400

    Add files via upload

---
+++ Wet Hands Minecraft.lua
@@ -0,0 +1,10 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*32 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+8 0 t y u y t w q e u o u t 8 0 t y u y t w q e u o u t [8a] w t y u y [ts] w [qp] e u o u t o p [8a] w t y u y [td] [wf] q [ep] u o u t f h [^J] 9 q [ej] [tg] e [qs] [9d] ^ 9 q e t [^J] 9 [qj] e [tg] e [qs] [9d] ^ 9 q e t s [8o] 0 t y u y t w 8 0 w t u o s f [9pdg] q e [tf] [us] [oh] [pj] 9 [qg] e t u d f [^g] 9 [qf] [Eg] t j [0wtf] y t [5y] 7 9 w r w 9 7 5 7 9 w r w 8 [^J] [9j] [qh] [eg] [th] [eg] [qh] [9j] 8 [0h] w t [ul] y t w [5k] [7h] [9d] [wa] [ro] [5d] [7a] [9o] [wy] r
+]]
