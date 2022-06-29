f56be153748bd4a07241093c58a3b33c779aaa77
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:53:45 -0400

    Add files via upload

---
+++ I'll never love again.lua
@@ -0,0 +1,30 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 4*30 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[djop] | [dj] | [dk] | [dk] | [dju] | [dj] | [dk] | [dk] | [djto] | [dj] | [dk] | [dk] | [sfyj] | [sfj] | [sfk] | u u | [wr] | w o I I u | y r e w |
+r r r r | r r r y | t | [au] | r r r r r e e | [tup] | [atu] |
+y u | r e w [yp] | [ay] | o I I u y r e w | [yp] | [ar] r r r r r r y [wt0] | [up] | [au] | r r u | r r e e |
+[tuo]a a a a p o u [ay] | [ey] | [wry] a a a a p o u [ryp] | o |
+[ry0]a a a [ayo] a a d [s8p] | [u8p] | [tuo] a a a [atuo] | a a [atu] | p s [sup] |
+[auo] d d d d a p o [yo]pa | [yp] | p [wy] [ay] | a a [ay] | o [yp] | [ryo] |
+[yo] a a a [ayo] a a d [sup] | [up] | u [atuo] a a a [atuo] | pas [sup] | [up] | y d | a p o | opa [ay] |
+f | a p o p a | y y u r | [ay] o o I I u y r e w | [ay] r r r r | r r r y | t | [au8] | u r r r r e | yu[eo] u y o u y o | a | [awy] | [ryp] | p p p o o |
+[ayo] | [ayo] | yu o | u o | [ro]uo | ou[ruo] o | y a | a a p p |
+y d | a p o [tu] o I [wru] | [auo] a a a [afo] | p p | o | u [ryo] | u o | u o | [ayo] | [ayo] | [ayo] |
+[sip] s s s [sip] | o o | o | s a o p [tuo] | yuyu |
+u a a a a f | p pap | u o | u o | u o |
+[sip] s s s [sip] | o o | s a o p o |
+a a a a | poua | a aaa | po u [yp] | o |
+a a a a a a d s | a a a a | a [atu] | p s s |
+[auo] d d d d s p o [euo] | p[ayo] | a a [afo] | p | [ryp] | o |
+[yo] a a a [ayo] a a d s | a [auo] a a a [uoa] | p s [suop] |
+y d | [atu] p o | opa [ayo] |
+[ryo] o p [ayo] | y [syo] | [syp] s s a [eyo] | [ruo] | 8 | a | f p [yp] | apouy | [tuo] | yo |
+p p I u I u [etu] | y y r r e u | y | r | [aty] u u y y r r | y u y e |
+[do] | r y y r | [to] | I u y | r | y | r | [e r y o]
+]]
