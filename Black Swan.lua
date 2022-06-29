514b4c3cfaccc94536a3d461cd791103702eb0da
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:55:23 -0400

    Add files via upload

---
+++ Black Swan.lua
@@ -0,0 +1,40 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*55 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[zji] g [zj] g [yjg] s [lg] s
+[pkf] s [kf] s [ojf] s [hf] s
+[jig] s [jg] s [yjg] s [lg] s
+[tkf] s [kf] s [jf] s [hf] [so4]a[p8] q ap[oi4] [o6] [oi8] o
+[so6]a[p0] e ap[o5] [s0] [wod] p
+[so4]a[p8] q ap[oi2] [o6] [o8] u
+[y3] [o5] [yo9] [p0] [wo] o o o [wt84] uuiui[ue862]iu|u
+[ut60]iuiuiu [50] ytuy
+[ut84]iuiuiu [862]|uiui
+[ut60]iu|yt[y50]ty uy
+[t84] uu[uq8]io [to862] oi[o86]u
+[ut60]uou[to0] o [to50]oou[to0]u
+[ut84]ioo[tqo8] oo[tpi862] oi[o86]u
+[wt60]yuu[wti0] ui[w50]u i u [84] [tp84][tp][p84]o[o84]u[ti862]u[u862]y[u862] [u862]
+[ut60] [p60]p[p60]o[o60]i[i50]u[u50]y[u50] [u50]
+[u84] [p84]p[h84] [pg84] [d862]d [s862]s[a862] [o862]
+[yd60]d [s60]s[a60] [p60]o[p50] [50]ou[50]
+[to4]o[p4]o[tqp8]o[tp4]o[tpi2]o[o8] [tqoe] [to8]
+[utp6]o[p6]o[tpe0]o[p6]o[tp5]o[o5] [wto0] [o5]
+[tpi4]o[tp8]o[qp]p[s8]s[id4]p[p8] [qpie] [pi8]
+[sp6][sp][sp][oa6] [e0] [sp6][sp][sp][oa5] [s5]s[wso0]p[p5]
+[tpi4]o[tp4]o[tqp8][tp][o4][tpi]2o[to8] [tqoe] [to8]
+[utp6]o[p6]o[tpe0][tp][o6][tp]5o[tp5]o[wto0] [to5]
+[tp4]o[p4]o[qp8]p[s4]s[id2]p[p8] [qpie] [pi8]
+[sp6][sp][sp][oa6] [e0] [sp6][sp][sp][oa5] [so5]s[wso0]pp [ts4]a[p8] q [a8]p[oi2] [o6] [oi9] [o6]
+[us6]a[p0] e [a0]p[o5] [s0] [wod] [p0]
+[so4]a[p8] q [a8]p[oi2] [o6] [o8] [u6]
+[y1] [o5] [yo9] [p0] [ywo] o o o [zji] g [zj] g [yjg] s [lg] s
+[pkf] s [kf] s [ojf] s [hf] s
+[jig] s [jg] s [yjg] s [lg] s
+[tkf] s [kf] s [jf] s [hf]
+]]
