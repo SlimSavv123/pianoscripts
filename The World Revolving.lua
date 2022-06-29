74e33fe8aff7608ee0bd9a9f186b59dcc6e8479b
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 12:11:00 -0400

    Add files via upload

---
+++ The World Revolving.lua
@@ -0,0 +1,48 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*01 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[j6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl4]%4[ig%]8%[OH4]%[vh4]%[gc4]%[sl9]%[zd5]%
+[xf6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl864]|[ig]|[pj] [xf975]|cx[zd]|
+[j6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl4]%4[ig%]8%[OH4]%[vh4]%[gc4]%[sl9]%[zd5]%
+[xf6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl864]|[zd]|[pj] [xf975]|cx[zd]|
+[t8] [64] 8 [64] 9t[r75]t[o9] [r75]t0 [86] 0 [86]
+7t[r53]e[w7] [r53] [t8]r[t64]o8 [o64]p
+[wo] [i80]uw [t80] [y6] [42]e6 [y42]
+[u0] [7%]iu[y0] [7%] [t8] [64] 8 [64]
+9t[r75]t[o9] [r75]t0 [86] 0 [86]
+7t[r53]e[w7] [r53] [t8]r[t64]o8 [o64]p
+[wo] [i80]uw [t80] [y6] [42]e6 [y42]
+[u0] [7%]iu[y0] [7%] 8t[r64]t[o8]t[r64]t
+[p9]t[r75]t[a9] [75] [s0]a[p86]o[u0]t[r86]t
+[I7]r[w53]r[o7] [53] [p8]o[i64]u[y8]t[y64]i
+[wu]t[w80]t[wu] [80]t[y6]e[q42]e[y6] [i42]
+[u0] [7%]iu[y0] [7%] 8t[r64]t[o8]t[r64]t
+[p9]t[r75]t[a9] [75] [s0]a[p86]o[u0]t[r86]t
+[I7]r[w53]r[o7] [53] [p8]o[i64]u[y8]t[y64]i
+[wu]t[w80]t[wu] [80]t[y6]e[q42]e[y6] [i42] [u3]|iu[y%]|
+[s8] [4%] 8 [4%] 8s[P4%]O[o8] [i4%]uw [80] w [80] 9u[i75]u[y9] [o75]
+[s8] [4%] 8 [4%] 8s[a4%]s[h8] [s4%]sw [80] w [80]ds[a9] [s75] [d9] [a75]
+[s8] [4%] 8 [4%] 8s[P4%]O[o8] [i4%]uw [80] w [80] 9u[i75]u[y9] [o75]
+[s8] [4%] 8 [4%] 8s[a4%]s[h8] [s4%]sw [80] w [80]ds[a9] [s75] [d9] [a75]
+[s8] [64] 8 [o64] [f9] [g75] [d9] [75]s[s0] [86] 0 [o86] [f9] [g75] [d9] [s75]
+[s8] [64] 8 [o64] [f9] [g75] [d9] [75]s[s0] [86] 0 [o86] [f9] [g75] [d9] [s75]
+[s8] [64] 8 [o64] [f9] [g75] [d9] [75]s[s0] [86] 0 [o86] [f9] [g75] [d9] [s75]
+[s8] [64] 8 [o64] [f9] [g75] [d9] [75]s[s0] [86] 0 [o86] [f5] g [d%] s
+[j6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl4]%4[ig%]8%[OH4]%[vh4]%[gc4]%[sl9]%[zd5]%
+[xf6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl864]|[ig]|[pj] [xf975]|cx[zd]|
+[j6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl4]%4[ig%]8%[OH4]%[vh4]%[gc4]%[sl9]%[zd5]%
+[xf6][x8][Z6][x8][Z0][x8]6[x8]6[x8][Z6][x8][Z0][x8]6[x8]
+[sl864]|[zd]|[pj] [xf975]|cx[zd]
+]]
