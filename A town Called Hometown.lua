040ae09650aa350860789c9247f515ecb9e08d03
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 11:54:19 -0400

    Add files via upload

---
+++ A town Called Hometown.lua
@@ -0,0 +1,93 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*59 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[sx] [fh] [fh]
+[sk] [fh] [fh]
+[sj] [gh] k [ghl]
+[sv] [gh] [gh]
+[sx] [fh] [fh]
+[sk] [fh] [fh]
+[sj] [gh] k [ghl]
+[sn] [gh] [gh]
+[sx] [fh] [fh]
+[sk] [fhv] [fhb]
+[sj] [gh] k [ghl]
+[gb] [fv] [dc]
+[gx] c x
+[hz] l
+l h g
+f [8of] [0w] [0wd]
+[7of] [0w] g [0wh]
+[6fl] [80] [80]
+5 [80dk] [80fl]
+[4dk] [89] [89sj]
+[3ah] [9w] [9wg]
+[2of] [68] g [2f]
+[5od] [%if]
+[6us] [0w] [0wa]
+[%is] [qw] d [qwf]
+[5ufk] [0w] [5ydj]
+[$tsf] s
+[4s]
+$ [Qf] [$s]
+[50of] g f
+[59id] w 9
+[8of] [0w] [0wd]
+[7of] [0w] g [0wh]
+[6fl] [80] [80]
+5 [80dk] [80fl]
+[4dk] [89] [89sj]
+[3ah] [9w] [9wg]
+[2of] [68] g [2f]
+[5od] [%if]
+[6us] [0w] [0wa]
+[%is] [qw] d [qwf]
+[5ufk] [0w] [5ydj]
+[$tsf] s
+[4s] $ [Qa] [$s]
+[50of] g f
+[59id] w [9x] z
+[el] t [uh]
+[wx] t c [uz] l
+[ql] t u
+q t [ux] z
+[el] t [uh]
+[wx] t c [uz] l
+[ql] [tm] [un]
+[qv] [tx] [uhz]
+[elx] t [uh]
+[wx] t c [uz] l
+[ql] [tl] [uz]
+[qx] [tc] [uhzv]
+[ejlx] t [uh]
+[wx] t c [uz] l
+[9l] Q [tj]
+[Qx] t z [ul] x
+[wz] t y
+[wv] [kx]
+[9lc] w [zv] [txb]
+[rzv] t y
+0 [wh] [th]
+[yh] g f g
+[qh] e [tl]
+[yh]
+0 [wh] [th]
+[yh] g f g
+[wh] [td] [ya]
+[us]
+0 [wh] [th]
+[yh] g f g
+[qh] e [tl]
+[yh] s d
+[9g] [qf] [es]
+[wo] s a
+[es] [ux] z [tl] z
+[wx] l z
+[gjc] x l
+h l k
+]]
---
+++ Before The Story.lua
@@ -0,0 +1,40 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*58 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+^|q|t|||
+8|w|y|||
+9|e|u|||
+9|e|i p| o| i|
+[s^]|q|t|||
+[d8]|w|y| |i|
+[u9]|e|u|||
+[i9]|e|i|||
+[s^]|q|[tl]| h| |
+[g8]|w|[yl]| s| |
+[s9]|e|[uf]| l||z|
+[l9]|e|[ih]| g| |
+[s^]|q|[tl]| h| |
+[g8]|w|[yl]| s| |
+[s9]|e|[uf]| l||z|
+[l9]|e|[ih]| g| |
+[so^4]|q|[tl]| h| |
+[sg85]|w|[yl]| s| |
+[sp96]|e|[ug]| l||z|
+[lg96]|e|[ih]| g| |
+[so^4]|q|[tl]| h| |
+[sg851]|w|[yl]| s| |
+[sp962]|e|[uf]| l||z|
+[lg962]|e|[ih]| g| |
+E|i|s|||
+t|o|d|||
+y|p|f|||
+y|p|g j| h| g|
+[lE]|i|s|||
+[zt]|o|d| |g|
+[yf]|p| s|j|J|h|g g
+]]
