5f3ca7b9a8551f9ae9b5d777a0fb78644ef85c96
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 01:55:50 -0400

    Add files via upload

--- song 1.lua
+++ song 1.lua
@@ -1,6 +1,6 @@
 shared.stop = true -- stops the player at any time if true
 -- CONFIG:
-shared.ftime = 5*60 -- time in seconds for the song to finish (extended by |)
+shared.ftime = 4*55 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- delay overides the delay
 shared.nospacedelay = true
