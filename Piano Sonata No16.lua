0442f95ad249670a4b11113c7a99afc8abe21326
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 14:12:18 -0400

    Add files via upload

---
+++ Piano Sonata No16.lua
@@ -0,0 +1,17 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 5*00 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[st] o u o [sf] o [uh] o [ya] o i [so]d[st] o u o
+[tj] p i p [ht] o [ul] o [hr] o [yg] [fo]g[tf] o u o
+[ip] asdfghjhgf[dtq]sap[ot0]
+pasdfghgfd[st0]apo[it9]
+opasdfgfds[ar9]poi[ut8]
+iopasdfdsa[p08]oiu[yeq]
+uiopaSdpaSdfgh[jq]klkjh[gw]f[ge]hjhgf[dQ]s[a5]7[h9]w[f5]8[s0]w[d5]7[h9]w[f5]8[s0]w[d5] | [hdaw] | [o5] |
+TyTyTyTytytytyty[rz]y[rk]y[rh]yryry[rj][yk][rj]y[rh]y[th]yr[yG][eG]yrytyrytyey[rz]y[rk]y[rh]yryry[rj][yk][rj]y[rh]y[th]yr[yG][eG]yrytyrytyeyzryoazkhftuosfhfletIpljGdryiadGdkwruokhfsetupsfsjQeyIjGdawryohda[put] [ut] [ut] [ut] [uts] [ut] [utf] [ut] [utj] [ut] [ut] [utk] [utl] [utj] [utl] [utj] [yk]a[oh]a[yz]aoayaoa[yl][ak][oj][ah][yj]sIsysIsysIsysIs[oah] hdhkzkhk[lspy]jGj[hoa] oyoadaoa[ste9]pIp[orw] | [kdw5] | [hdw5] | [ow5] hdhJzJhJ[lspy]jGj[hPo] oyoPdPoP[ste9]pIpoweEtyuIohJjhgfdSertyuIOpLxzLJjh[g92] dpdgjgdg[houe]fSf[diy] yeyipiyi[ow06]uTu[9q] yuiopaSdyiuytreWasdfGHjkWreWq098jxzlkjhgypoiuytrhzlkjhgftoiuytreglkjhgfdriuytreWfkjHgfdsetrewq09PdsPpoi[uEw8]iopPsdf[gi] s p s [ij] s [pl] s [of] s P [sg]h[ig] s p s [iz] d P d [il] s [cp] s [ul] s [oJ] [sj]J[ij] s p s [Pd] fghjJlzlJj[hiE]gfd[sie] dfghjJlJjh[gie]fds[Piw] sdfghjJjhg[fwu]dsP[piq] PsdfghjhgfdsPpjqweEtyuiuyt[Esj]ewq[0sh] qwertyuytr[esh]wq0[9sg] 0qwertytre[wag]q09[8sf] 90qwertrew[qsf]098[dqe]yuiopaSdpaSdfgh[jq]klkjh[gw]f[ge]hjhgf[dQ]s[5a]7[9h]w[5f]8[0s]w[5d]7[9h]w[5f]8[0s]w[5d] | [whda] | [5o] |
+]]
