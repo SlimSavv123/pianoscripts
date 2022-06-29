ea4d5ab288edf42e67d57b0f6e55232634bb1c57
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 19:23:13 -0400

    Add files via upload

---
+++ Christina.lua
@@ -0,0 +1,10 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*59 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[D8] g [ohYP]||J j|t|[pg]|[h8]|[oYP]| |t|[pi]| |[D8] g [ohYP]||J l|p|[ji]|[h8]|[oYP]| |t s P p i|[s8]dDg [ohYP]||J j t i o [pg]|[h8]|[oYP]| |t|[pi]| |[d8]D g [ohYP]||J l s P [pg] [iJ] j [h8]|[oYP]|d|[pD] [o4] [d8] [s(] g s [d%]|[wtE]||h [h%]|[tiW] g [YJ] H [h5]|[wY(]|i d [d@] [D(] [wd] y [PE]|[d%]|[wti]||h [h%]|[toW] h J l [h5] 9 e E i [hZP] [zg5] [dJ] [hPE]yi [wH]|[s4]gJ [qYWH]|[qYW] [hD] [gH4] 8 [qYW]|[qYW] l [lh5]|[wigJE]|[wiE] j [dJ5] 9 [oE] e [ohdE] z [D%]Hz [liWD(] Y [hD]|[ztg]YP [hZO] o [icH]|[zywthg]|5 5 5|[ia5]| |[Y1] i [woE(]||P [p8] w e|[qi]|[o1]|[w(]| |8|[qe]|4|[Y1] i [woE(]||P [s8] w e|[qp]|[o1]|[wE(]| |8 [tp] [oE] [ie] [tq]|[w1]tYi [woE(]||[yP] [pi] 8 q w [ie]|[o1]io [wE(]| |8|qete Ety[Y1] i [woE(]||[YP] [si] q e [ti] [iP] p [o8] [tY] [wt] [PE] Y o [tpi4] [oP] [tsqpe] [dP] [tsqeD] [gd] [hD%] [sD] [tsYWO] [hD] [sD] [sO] [hD] [sD] [sO] [hD] [sD] [sO] [jg^] [gd] [yedPE] [jg] [gd] [dP] [jg] [ged] [dPE] [tjg] [ygd] [idP] [lH*] [gH] [igWTS] [lH] [gH] [gS] [lWH]To[gH] [gS] [lH] [igH] [gS] [zJ@] [hJ] [whD]Ey[zJ] [hJ] [hD] [ziJ] [hJ] [hD] [zJ] [hZ] [gc] [ywhE]Jzv
+]]
