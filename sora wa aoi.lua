ae3dafc6b86a8890f1c73c6bad27c547db3f3929
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 02:55:27 -0400

    Add files via upload

---
+++ sora wa aoi.lua
@@ -0,0 +1,27 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*45 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[QSj]TpSkj[0aH]r[TO] Sf[9pG]efy
+S [TI9p] 90 [pS[$j]]*Q[WS][ek]j[3aSH]7
+*WSf[9pG]e[yaH]u[ISj] [aSG[yk]] j6
+[fjk[2x]] dSp[9u] y e [6S]0[eT]
+[Tea]S[3f]0[Wr] [Wr]O[2O]9[Qep] [Qe]T[2y]6
+[9QT] [Q9e]r[6S]0[eT] [Teua][pS][O3f]0[Wr] [Wr]j [2aH]9[epQG]u[eQI]f[2S][6I][9QT] 2[!f][7G]7[Qrf]T
+[Qry]I[Qrp] 7 7S[3G]7[W0f]I[0WO]a[W0f]H
+[7k] 4a[$a]*[eS]Tup[3a]7[0O]Wua
+[2a]6[QS]TuTp[rO][eI][9T][3ua]p[$a]*[eS]T
+uI[3O]*Wurp[I2ya]6[yQIS]6[29yIS][6d][i!*TS]% [*W][qT][Wi][qO][i*TS]%[!*Tia] [7iTS] [6upTS]0eT[ea][0S][%Ouaf]0Wr[WS][0f][p$SH]*
+[QSGpj]eTI[eTI][OH][eQTIG][uf][*Qeyd][TS][7adIG]Qry[rig][QIG]
+[s^IPG]QEy[EOH][QIG][7pfIG]QeyeQ[dg%OH]9qW
+[rIG][WOH][$SGpj]*Qe[Quf][*IG][3fOSH]*[G[0fOSH]]W0*[G2pdj]6
+9Q[9aOH][6pIG][6pISG]0[Seupf]T[Sepfj]0[G7pdj]Qr[yG][IGz][yfL]
+[rdj][Qp][9I][7p][$I][7pG][3pISG] [Spf] [3yup]T[4ryip]ryi
+[4yp]a[p$IS]rTp[$a]S[3pf] [upS] [T3up] [2eyI]r
+Tp[2a]S[2pf]$[6upS] [T3up] [e$TI]rTp[$a]S
+[3pf] [upS] [3pSH] [2pdG][6r][9T][Qp][9a][6S][I9pf] [I2rya] [IO3ra] [TIOP[$S]]
+]]
