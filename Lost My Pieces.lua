4a7687e8961de4a77756bdc827c03f33e98a9881
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:27:25 -0400

    Add files via upload

---
+++ Lost My Pieces.lua
@@ -0,0 +1,41 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*03 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[xhe] [us] [oa] h||j|
+[wf] t u o s| |
+[qf] [tg] [uf] [pd] s a s d
+[o0] r y o a| s|
+[qpd] t y i a| d|
+[h0] r y u [od]| |
+[f7] Q r I [aD]| |
+[fO0]| r| u| O|
+[p9] e [ta]| [s0] t o|
+[qp] t [ia]| s| d|
+[fe] t u s j| |
+[dW] r y I x| c|
+[vq] [tb] [vu] m [pn] v z b
+[wv] r y o z| c|
+[xe] c [xt] b [vp] l z c
+[xw]| y| [ol]| h|
+[vq] b [vt] m [pn] v z b
+[wv]| [ym]| z| c|
+[xme0]| [vm]| [zwv9]| [zk]|
+[qk] t p s| |q t y o||a| [s0]|||
+[ve] l [uk] v||b|
+[xw]| u| [xl]| |
+[xq] c [xt] z [pli] k l z
+[h0]| r| [uok]| l|
+[zqj]| i| [kd]| z|
+[v0]| [uo] z [da]| |
+[xu7]| |[ZD] I a|
+[xH0] r u O f| g|
+[qh] [tj] [uh] l [pk] h d j
+[wh] r y o z| c|
+[xme] t [vpm]| [zwv] y [zok]|
+[qj] t o s a| |i s d h| |f| |k| l| g l z v
+]]
