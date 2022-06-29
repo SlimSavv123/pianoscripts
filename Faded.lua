693f0c9cd032bba76ded81665cd638305aec6e0e
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:42:07 -0400

    Add files via upload

---
+++ Faded.lua
@@ -0,0 +1,62 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 3*43 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[0o] r [wo] r [0o] r [wa] r
+[8f] w [0f] w [8f] w [0d] w
+[wa] y [ra] y [wa] y [ra] y
+[9I] e [QI] e [9I] e [Qu] e
+[0wr] o [0wro] u [0wro] u [0wro] p [80wa]| [80wo] o [80wy] a [80w]|
+[wry]| [wry]| [wry] o [wryo] o [9eQI]| [Q9e] I [9eQI] u [Q9eo]|
+[0wr] o [0wro] u [0wro] u [0wro] p [80wa]| [80wo] o [80wd] a [80w]|
+[wry]| [wry]| [wry] o [wryo] p [Q9e]| [Q9e]| [Q9e] a [Q9ea] a
+[0a] r w r 0 r w r 8 w 0 w 8 [wa] [0a] [wa]
+[wa] y r y w y r y 9 e Q e 9 [ea] [Qa] [ea]
+[0a] r w r 0 [ro]p[wp] [rp]a[8p] [wo] [0o] w 8 [wa] [0a] [wa]
+[wa] y r y w [yo] [ro] [yp]
+[9I] [eI] [QI] [eo] 9 [ea] [Qa] [ea]
+[0wroa]| [0wro]| [0wro]| [0wra]–
+[80wf]| [80wf]| [80wf] o [80wd] da[wry]| [wry]| [wry] o [wryd] da[Q9e]| [Q9e] p [Q9e] a [Q9ea] a
+[0a] r w r 0 [ro] [wo] [ru]s8 w 0 w 8 w 0 [wd]
+[wa] [yd] [ra] [yd] [wa] [yd] [rp] [yo]s9 e Q [eo] [9s] e [Qa] e
+[0o] [ro] [wo] [ro] [0a] [ra]a[wh] [rG]
+[8f] [wf] [0f] [wf] [8d] [wd] [0p] [wp]
+[wa] [ya] [ra] [ya] [wd] [ys] [ra]p[yo]
+[9I] [eI] [QI] [eI] [9p] [ep] [QI] [eI]
+[0o] [ro] [wo] [ro] [0a] [ra]a[wh] [rG]
+[8f] [wf] [0f] [wf] [8d] [wd] [0p] [wp]
+[wa] [ya] [ra] [ya] [wd] [ys] [ra]p[yo]
+[9I] [eI] [QI] [eI] [9o] [eo] [Qp] [eo]
+[0wr] o [0wro] u [0wro] u [0wro] p [80wa]| [80wo] o [80wy] a [80w]|
+[wry]| [wry]| [wry] o [wryo] o [9eQI]| [Q9e] I [9eQI] u [Q9eo]|
+[0wr] o [0wro] p [0wro] u [0wro] p [80wa]| [80w] o [80wd] a [80w]|
+[wry]| [wry]| [wry] o [wryo] p [Q9ep]| [Q9e]| [Q9e] a [Q9ea] a
+[0a] r w r 0 r w r 8 w 0 w 8 [wa] [0a] [wa]
+[wa] y r y w y r y 9 e Q e 9
+[eo] [Qd] [ed]a0 [ro] [wd] [rd]a0 [ro] [wd] [rd]p8 [wo] [0o] [wp] 8
+[wo] [0d] [wd]aw y r y w [yo] [rd] [yd]a9 e Q [ep] 9 e Q e
+[0o] [ro]p[wo] [ro]d[0a] [rI] [wo] [ro]
+[8o]o[wo]p[0o] [wo]d[8a] [wI] [0o]o[wo]o
+[wo]o[yo]p[ro] [yo]d[wa] [yI] [ro]o[yo]o
+[9o]o[eo]p[Qo] [eo]d9 [ea] [Qa] [ea]
+[0a] r w r 0 r w [ro]
+[8u] [wy] 0 w 8 [wo] [0d] [wd]aw y r y w [yo] [rd] [yd]a9 e [Qp] e 9 [ea] [Qa] [ea]
+[0a] r w r 0 [ro] [wu] [ry]s[8s] w 0 w 8 w 0 [wd]
+[wa] [yd] [ra] [yd] [wa] [yd] [rp] [yo]s9 e Q [eo] [9s] e [Qa] e
+[0o] [ro] [wo] [ro] [0a]a[ra] [wh] [rG]
+[8f] [wf] [0f] [wf] [8d] [wd] [0p] [wp]
+[wa] [ya] [ra] [ya] [wd] [ys] [ra] pyo
+[9I] [eI] [QI] [eI] [9p] [ep] [QI] [eI]
+[0o] [ro] [wo] [ro] [0a]a[ra] [wh] [rG]
+[8f] [wf] [0f] [wf] [8d] [wd] [0p] [wp]
+[wa] [ya] [ra] [ya] [wd] [ys] [ra] pyo
+[9I] [eI] [QI] [eI] [9o] [eo] [Qp] [eo]
+[0o] [ro] [wo] [ro] [0o] [ro] [wa] [ra]
+[8f] [wf] [0f] [wf] [8f] [wf] [0d] [wd]
+[wa] [ya] [ra] [ya] [wa] [ya] [ra] [ya]
+[9I] [eI] [QI] [eI] [9I] [eI] [QI] [eI]
+]]
