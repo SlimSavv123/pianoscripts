e7aa6b50dc978e6482da556857250390a35c3ad0
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 11:38:16 -0400

    Add files via upload

---
+++ Determination.lua
@@ -0,0 +1,10 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 0*48 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+g [9f] [qd] [ws] [ed] [qp] a w [ro] y [rd] [wf] g 9 [0h] q [wl] 0 [qj] 0 9 8 9 6 g [9f] [qd] [ws] [ed] [qp] a 8 [qo] w [ey] [wu] i 9 [wu] e [qt] 8 [qy] 0 9 8 9 0 g [9f] [qd] [ws] [ed] [qp] a w [ro] y [rd] [wf] g 9 [0h] q [wl] 0 [qj] 0 9 8 9 6 g [9f] [qd] [ws] [ed] [qp] a 8 [qo] w [ey] [wu] i 9 [wu] e [qt] 8 [qy] 0 9 8 9 0 [Dh] [^dg] [Dws] [Pqd] [eps] [PDw] [qpd] w [9ip] 0 [8ip] [pd] [Dh] [^dg] [Dws] [Pqd] [eps] [PDw] [qpd] [ey] [yo] [ps] [6oa] [8ip] [7uo] [wip] [0oa] [9ip] 0 [qey] 0 [qey] [wtu] [qyi] [ip] [Ped] [Swp] q 0 9 * 0 [Dh] [^dg] [Dws] [Pqd] [eps] [PDw] [qpd] w [9ip] 0 [8ip] [pd] [Dh] [^dg] [Dws] [Pqd] [eps] [PDw] [qpd] [ey] [yo] [wps] [oa] [ip] [uo] [ip] [oa] [9ip] [qy] [qy] [wu] [6ei] [8wu] [60t] [9qy]
+]]
