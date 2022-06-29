3f97b8c58bdf07a9da988520ceef9b0faec650b1
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 19:11:16 -0400

    Add files via upload

---
+++ Bloody Tears.lua
@@ -0,0 +1,50 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 4*55 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[pasf] j x l k j x l k j x l k j x l [asfk] j x l k j x l k j f s a p u t
+6 0 [ep] 0 [ra] 0 [ts] 0 [ra] 0 [wo] 0 [ra] 0 [ep]
+4 8 [qp] 8 [ea] 8 [qs] [5d] 9 [ws] 9 [ra] 9 [wp]
+6 0 [ep] 0 [ra] 0 [ts] 0 [ra] 0 [wo] 0 [ra] 0 [ep]
+[4 p s f] 8 q [8d] e 8 [qs] [5a] 9 s [wd] 9 [rdh] |
+[6j] f x f z f l f k f l f k f j f [5k] f l f z f l f k f h f k f j f
+[4j] f x f z f l f k f l f k f j f [5k] f l f z f l f k f h f k f j f
+[60tj] f x f z f l f k f l f k f j f [59wk] f l f z f l f k f h f k f j f
+[4qj] f x f z f l f k f l f k f j f [5wk] f l f z f l f k f h f k f j |
+[ead] [udh] [psf] a s p u d [es] y [w y o a d] y [ofh] p [af]
+o y w [d f d] [qs] t i o i p i t d [qs] [O0a] r u s O
+[ad] r [O0dg] 7 [6psf] 0 e r t e 0 [fj] [6k] l [5adk] j [9h] [adk] w j [rh]
+y 9 7 5 [f j l x] 4 8 q w q 8 [egjlc] q 8 4 w [H3fkx] 7 0 [Wz] [0l]
+[30k] [30l] [18z] [7k]
+[6p] u [0f] u [td] u [rs] u [ta] u [rs] u [ea] u [0p] u
+[5a] u [9s] u [ed] u [ws] u [ra] u [eo] u [wa] u [9p] u
+[4p] u [8f] u [wd] u [qs] u [ea] u [ws] u [qa] u [8p] u
+[5a] u [9s] u [ed] u [ws] u [ra] u [wo] u [ya] u [wp] u
+[6p] u [0f] u [td] u [rs] u [ta] u [rs] u [ea] u [0p] u
+[5a] u [9s] u [ed] u [ws] u [ra] u [eo] u [wa] u [9p] u
+[4p] u [8f] u [wd] u [qs] u [ea] u [ws] u [qa] u [8p] u
+[5wa] u [5ws] u [5wd] u [5ws] u [7ra] u [7ro] u [5wa] u
+[30p] u [6ead] [0dh] [esf] t r [6e] 0 6 [wd] [6s] 0 [5wad] 9 [wfh] e [wsf]
+[5w] e w [rad] [wps] 9 [4qad] [8dh] [qsf] w q [4q] 8 e
+[4qd] [4qf] [370adg] [370] [370afh] [370] [370]
+[%O30af] [O70rag] [6ead] [0dh] [esf] t r [6e] 0 6 [wd]
+[6s] 0 [5wad] 9 [wfh] e [wsf] [5w] e w [rad] [wps] 9
+[4qad] [8dh] [qsf] w q [4q] 8 e [4qd] [4qf] [370adg]
+[370] [370afh] [370] [370] [%WOaf] [30afH] [29afj]
+[H7fk] [6fk] 0 [tfj] r e [5fk] 9 [rfj] e w [4fk] 8 [efj] q 8
+[30fl] [7fk] [rfj] [0fh] [6fk] 0 [tfj] r [euf] [5fk] 9 [rfj] e [wuf]
+[4fk] 8 [efj] q [8pf] [30fl] [30fz] [H18fk] [H7fl]
+[6ej] f [0x] f [tz] f [rl] f [tk] f [rl] f [ek] f [0j] f
+[5wk] f [9l] f [ez] f [wl] f [rk] f [eh] f [wk] f [9j] f
+[4qj] f [8x] f [wz] f [ql] f [ek] f [wl] f [qk] f [8j] f
+[5wk] f [9l] f [ez] f [wl] f [rk] f [wh] f [yk] f [wj] f
+[6ej] f [0x] f [tz] f [rl] f [tk] f [rl] f [ek] f [0j] f
+[5wk] f [9l] f [ez] f [wl] f [rk] f [eh] f [wk] f [9j] f
+[4qj] f [8x] f [wz] f [ql] f [ek] f [wl] f [qk] f [8j] f
+[5wk] f [9l] f [ez] f [wl] f [rk] f [wh] f [yk] [wj]
+[e j l x] j [ul] j [az] j x j [wz] h [yk] h [pv] h x c [qlx] t o p [5ad] 9 [eoa] [ps] r [ad] [6f] p [0s] p [rd] p [0f] p [5d] o [9a] o [eh] o [9f] g [4pf] 8 w [ei] o [5ip] 0 | [6tup]
+]]
