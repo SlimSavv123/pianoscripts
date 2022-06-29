049d3d208e4dbdac9321dd4a92dff24353f0b882
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 02:38:49 -0400

    Add files via upload

---
+++ pLAY SHALLOW.lua
@@ -0,0 +1,84 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 3*42 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+6 [0t] o y [7r] [wyo]| [8tu]|||
+q t i o t q p o
+8 [to]| [5ro]| [9p]o[wy] [5t]
+6 [0t] o y [7r] [wyo]| [8tu]| w t| |8|
+6 0 [etp] [tp] [7tp] [wo]| [8tu]| w t| |
+q t i [qti] [ti] [ti] [qti] [ti]
+[8ti] u| [5wty]| 9 [wp]o[5y]
+6 0 [etp] [tp] [7tp] o w [8tu]| w t||w t 8
+4 8 [qti] [eti] [ti] [4ti] [qti] [4ti]
+[8ti] u| [5wty]| 9 [wp]o[5y]
+[60et]| |[7wry]||[8wy] [8tu]|||
+4 [8to] [qto] [eto] [to] i 4 u
+[8ty]| u [5ry]| [9y] [wt] [5t]
+[60et]| |[7wry]||[8wy] [8tu]|||
+4 [8to] [qto] [eto] [to] i 4 u
+[8ty] w u [5wy]| 9 w 5
+6 [0t] o y [7r] [wyo]| [8tu]| w t| |8|
+6 0 [esj] [tsj] [7sj] [7h] r [8osf]| w u||w t|
+q t [is] [pg] [pg] [tpg] [ipg] [pg]
+[8pg] [wof] t [5sd]| [9f]d[ws] [ra]
+6 [0s] [ej] [tsj] [7h] w [rf]d[8os] w u||w t 8 4
+8 [qs] [epg] [tpg] [pg] [4qpg] [pg] [8tog]
+[wf] t [5wosd]| 9 w 5 [60uos]| |
+[7wryod]||[8wtuof]|||
+4 [8osh] [qosh] [eosh] [tosh] [og] 4 f
+[8osd] w [tf] [5wsd]| 9 [wd] s
+6 0 e t [7wrosh]||[8wtosf]||w| w t 8 4|
+[8osh] [qosh] [eosh] [tosh] g 4 f
+[8osd] w [tf] [5wd]| [9s] w 5
+[29gjc] [gjc] [gjc] [9ygjc]| [fjx]||
+[5wdhz] [dhz] [fhx] [wrydhz]| [sl] [5w]|
+[8tfhx] [fhx] [fhx] [hv] [7wr] [sfl]| [sfl]
+[60e] [fx]| |[6e]|
+[29gjc] [gjc] [gjc] [9ygjc]| [fx] [29]|
+[5wdhz] [dhz] [fx] [wrydz]| [sl] [5w] [oh]
+[8wtoh] [fhx] [tuofhx] [hxv] [7wr] [sl] [wry] [sfl]
+[60e] [fx] [etu]||6 e 6
+2 9 [edgl] [ydgl] [2dgl] [dl] 9|
+[5wdhk] k l [wryhk]||[5w]|
+8 w [tfhl] [fhl] [7dhl] w [rdhl] [7k]
+[6fhk] [0j] e [sfj]| 0 e 6
+[29]| [dgl] [9edgl] [dgl] [dl] [29]|
+[5wdhk] k l [wryhk]||[5wh] h
+[8tfhl] [fhl] [tuofhl] [dhl] [7r] h [wry] [sf]
+[6eu]||[tu]| [et] u e
+6 0 e t u 0 e 6
+[3uoa] 0 w r [uoa] [rs] [0a] 3
+[5yoa] 9 w 5 9 w 5 [I9ya]
+2 9 e [ys] e [9f] [9s] [ea]
+[6tup] 0 e t [upsf] 0 e [6uoa]
+3 0 w [ruoa] 0 [rs] [0a] [3s]
+[5oaf] 9 w 5 9 w r 5
+[I2pf] 9 e 9 e y 9 e
+[29gjc] [gjc] [gjc] [9egjc]| [fjx]||
+[5wdhz] [dhz] [fhx] [wrydhz]| [sl] [5w]|
+[8wtfhx] [fhx] [tosfhx] [hv] [7wr] [sl] [wry] [sfl]
+[60e] [fx] [etu]||6 e 6
+[2gjc] [9gjc] [egjc] [ygjc] 9 [efx] y 9
+[5dz] [wdz] [rfx] [ydz] w [ysl] r [5oh]
+[8sfl] [wsfl] [tsfl] [8sfl] 7 [woh] r [7uf]
+[6u] 0 e t u 6 e 6
+2 9 [edgl] [ydgl] [9dgl] [edgl] y 9
+[5hk] [9k] [wl] [9dhk] r 9 w 5
+8 w [tfhl] [8fhl] [7dhl] [wl] [rdhl] [7k]
+[6fhk] [0j] e [tfj] u| 6|
+2 9 [esgj] [ysgj] [9sgj] [ysgj] e 9
+[5adh] 9 [wdj] [rdk] y| w h
+[8fhl] [wfhl] [tfhl] [dhl] 7 [wh] r [7sf]
+6 0 e t u e t 0
+[9eygjc] c c c| x||
+[wyodhz] z x z| l| h
+[tuofhx] x x v [roa] [hl]| [hl] [eu] x| |
+[ypdgjc] c c c| x| u
+[oadhz] z x z| l| h
+[tosfl] l l l [roa] h| f [eup]
+]]
