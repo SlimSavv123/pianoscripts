e6d75bd7a8cce46759379bdaf7dfe3487ce7aaf4
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 12:01:36 -0400

    Add files via upload

---
+++ Don't Forget.lua
@@ -0,0 +1,27 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*21 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[sf]| [sg]| [qp] s h [th]|[ih]|[oh]|h|
+s|f|[o0] s d [td]|[yd]|d|d|
+f|d|[sq]|[ts]|[si]|[so]|s|
+k|l|[spkY]| j k j| f|
+[sf]|[sg]|[p9] s h [he]|[th]|[yh]|h|
+s|f|[o0] s d [ed]|[td]|[yd]|[od]|
+f|d [sq] e y t|i|o|[wrf9]|g|[rdW9]|s|[te0]|s||d H l z|||
+f|d|[s9] q e t| o|[wf]|g|[qd]|s|t| s|||
+[e4] t y o|||
+[w3] t y|||
+[t4]|8|q|w| |
+[tea(]|||
+[te(]|||
+[p9] s h|e|t|y| t|9|
+[o0] s d e|t|y|o| |
+[sq] e y t|i| [wrf9]| [rdW9]|
+[te0]| |[ytW9]|||
+y i p s||5| %| 6|0|r|t|o|a|s|h|k|l|v
+]]
