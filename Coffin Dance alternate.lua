c21cdad10fc031537874885d500f30d3d62c531c
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:32:57 -0400

    Add files via upload

---
+++ Coffin Dance alternate.lua
@@ -0,0 +1,26 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 1*20 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+[6s] s [6s] s [6f] f [6f] f
+[6d] d [6d] d [6h] h [6h] h
+[6j] j [6j] j [6j] j [6j] j [6ej] [6ej] [6ej] [6ej]
+[6ed] s a o
+[4qp] [4qp] f [4qd] [4qs] [5wa] [5wa] a [5wd] [5ws] a
+[6ep] [6ep] l [6ek] l [6ek] l [6ep] [6ep] l [6ek] l [6ek] l
+[4qp] [4qp] f [4qd] [4qs] [5wa] [5wa] a [5wd] [5ws] a
+[6ep] [6ep] l [6ek] l [6ek] l [6ep] [6ep] l [6ek] l [6ek] l [4qp] [4qp] f [4qd] [4qs] [5wa] [5wa] a [5wd] [5ws] a
+[6ep] [6ep] l [6ek] l [6ek] l [6ep] [6ep] l [6ek] l [6ek] l
+[4qs] s s s f f f f
+[5wd] d d d h h h h
+[6ej] j j j j j j j j j j j
+d s a o
+[4qp] [4qp] f [4qd] [4qs] [5wa] [5wa] a [5wd] [5ws] a
+[6ep] [6ep] l [6ek] l [6ek] l [6ep] [6ep] l [6ek] l [6ek] l
+[4qp] [4qp] f [4qd] [4qs] [5wa] [5wa] a [5wd] [5ws] a
+[6ep] [6ep] l [6ek] l [6ek] l [6ep] [6ep] l [6ek] l [6ek] l 6
+]]
