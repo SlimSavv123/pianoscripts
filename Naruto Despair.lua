bd7378c5d30751135762be174ba8041176fd4070
SlimSavv123 <108383625+SlimSavv123@users.noreply.github.com>
2022-06-29 03:09:06 -0400

    Add files via upload

---
+++ Naruto Despair.lua
@@ -0,0 +1,10 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*13 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+shared.nospacedelay = true
+
+shared.scr = [[
+0|r|e| |0|r|Q| |0|[rI]|[wo]|[ra]|[I9]|r|Q|r|[I8]|w|[p0]|e|[o0]|r|w|r|0|[rI]|[wo]|[ra]|[I9]|r|[dQ]|r|[f8]|[wo]|[o0]|[pe]|[a0]|r|w|r|8 w [tGC] [vh] [GC8]|[xwf] [zd] [ka0]|w|Q|0|9 e [yGC] [vh] [GC9]|[xfe] [zd] [xf0] r o|I|u|8 w [tGC] [vh] [GC8]|[xwf] [zd] [ka0]|w|Q|0|9 e [yGC] [vh] [GC9]|[xfe] [zd] [xf0] r o|I|u|0|r|e| |0|r|Q| |0|[rIG]|[woh]|[rka]|[IG9]|r|Q|r|[IG8]|w|[pj0]|e|[oh0]|r|w|r|0|[rIG]|[woh]|[rka]|[IG9]|r|[zdQ]|r|[xf8]|[woh]|[oh0]|[pje]|[ka0]|r|w|r|8 w [tGC] [vh] [GC8]|[xwf] [zd] [ka0]|w|Q|0|9 e [yGC] [vh] [GC9]|[xfe] [zd] [xf0] r o|I|u|8 w [tGC] [vh] [GC8]|[xwf] [zd] [ka0]|w|Q|0|9 e [yGC] [vh] [GC9]|[xfe] [zd] [xf0] r o|I|u|0|r|e| |0|r|Q| |0
+]]
